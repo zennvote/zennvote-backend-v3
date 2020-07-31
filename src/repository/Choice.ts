1f466e4580ac8af080234c8d14aea6f43dc6109b
XxshiftxX <contact@dev-shift.me>
2020-07-31 15:49:40 +0900

    Choice Repository 추가

---
+++ src/repository/Choice.ts
@@ -0,0 +1,9 @@
+import Choice from '@src/domain/Choice';
+
+export const getChoices = (): Choice[] => {
+  throw new Error('Not Implemented');
+};
+
+export const getChoiceByName = (name: String): Choice | null => {
+  throw new Error('Not Implemented');
+};
