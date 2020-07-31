8b7210a16a3ac9a8dfde08ce0d91b0311e4a651f
XxshiftxX <contact@dev-shift.me>
2020-07-31 18:44:03 +0900

    임시 라우트 연결

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,11 @@
 import * as express from 'express';
+import { GetChoicesHandler, GetChoiceByNameHandler } from './handler/Choice';
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/v1/choice', GetChoicesHandler);
+app.get('/v1/choice/:name', GetChoiceByNameHandler);
 
 export default app;
