f20722c92d1c53c7e490765ac32b5dd942a75013
XxshiftxX <contact@dev-shift.me>
2020-08-03 10:27:44 +0900

    배포 자동화를 위한 파일 작성

---
+++ clear.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf /home/ubuntu/zennvote/backend-v3/deploy/server/zennvote-backend-v3
---
+++ execute-deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ubuntu/zennvote/backend-v3/deploy/scripts
+sudo ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
