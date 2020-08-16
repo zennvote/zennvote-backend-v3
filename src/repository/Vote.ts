e152b16b9d7cace644831bf8446600f63dec5274
XxshiftxX <contact@dev-shift.me>
2020-08-17 00:09:00 +0900

    Vote Repository
    - isEmailDuplicated 작성

--- src/repository/Vote.ts
+++ src/repository/Vote.ts
@@ -1,5 +1,9 @@
 import Vote from '@src/domain/Vote';
 
+export const isEmailDuplicated = async (email: string): Promise<boolean> => {
+  throw new Error('Not Implemented');
+};
+
 export const saveVote = async (vote: Vote): Promise<Vote> => {
   throw new Error('Not Implemented');
 };
