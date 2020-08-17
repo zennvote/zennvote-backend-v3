c3c2eec813dce356545669dcbef0aa18648f75a8
XxshiftxX <contact@dev-shift.me>
2020-08-17 23:28:06 +0900

    Vote handler
    - 이메일 중복 케이스 대응

--- src/handler/Vote/index.ts
+++ src/handler/Vote/index.ts
@@ -1,10 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 
 import * as VoteApp from '@src/application/Vote';
+import * as VoteError from '@src/application/Vote/errors';
 
 export const PostVoteHandler = async (req: Request, res: Response, next: NextFunction) => {
   const vote = req.body;
-  const result = await VoteApp.saveVotes(vote);
 
-  res.json({ success: true, result });
+  try {
+    const result = await VoteApp.saveVotes(vote);
+
+    res.json({ success: true, result });
+  } catch (error) {
+    if (error instanceof VoteError.VoteDuplicatedError) {
+      res.status(422).json({ success: false, message: 'vote email already existing' });
+    } else {
+      next(error);
+    }
+  }
 };
