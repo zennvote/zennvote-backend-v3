ae8e1cc49ac397bea84c7efa0815db795ceb69b4
XxshiftxX <contact@dev-shift.me>
2020-08-17 23:19:18 +0900

    Vote Application
    - 이메일 중복 케이스 대응

--- src/application/Vote/index.ts
+++ src/application/Vote/index.ts
@@ -1,4 +1,11 @@
 import * as VoteRepository from '@src/repository/Vote';
 import Vote from '@src/domain/Vote';
+import { VoteDuplicatedError } from './errors';
 
-export const saveVotes = (vote: Vote) => VoteRepository.saveVote(vote);
+export const saveVotes = async (vote: Vote) => {
+  const isDuplicated = await VoteRepository.isEmailDuplicated(vote.email);
+  if (isDuplicated) {
+    throw new VoteDuplicatedError(vote.email);
+  }
+  return VoteRepository.saveVote(vote);
+};
