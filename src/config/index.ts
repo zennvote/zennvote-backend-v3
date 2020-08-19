3e2468b5650d9da1837bce1d0ba8cc57bc475109
XxshiftxX <contact@dev-shift.me>
2020-08-19 16:33:57 +0900

    lint 적용

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,10 +4,10 @@ config();
 
 export default {
   // Express
-  expressPort: parseInt(process.env.EXPRESS_PORT ?? '4000'),
+  expressPort: parseInt(process.env.EXPRESS_PORT ?? '4000', 10),
   // MySQL
   mysqlHost: process.env.MYSQL_HOST,
-  mysqlPort: parseInt(process.env.MYSQL_PORT ?? '0'),
+  mysqlPort: parseInt(process.env.MYSQL_PORT ?? '0', 10),
   mysqlUser: process.env.MYSQL_USER,
   mysqlPassword: process.env.MYSQL_PASSWORD,
   mysqlDatabase: process.env.MYSQL_DATABASE,
