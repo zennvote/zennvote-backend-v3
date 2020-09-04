30af0010562d6de668171ac00d4a9959d4f636c0
XxshiftxX <contact@dev-shift.me>
2020-09-04 12:49:45 +0900

    FIX: Quiz repository
    - 미구현 함수 구현

--- src/repository/Quiz.ts
+++ src/repository/Quiz.ts
@@ -1,9 +1,67 @@
 import Quiz from '@src/domain/Quiz';
+import { getConnection } from '@src/infrastructure/db/connection';
+import { RowDataPacket } from 'mysql2/promise';
 
-export const getQuizzes = (): Promise<Quiz[]> => {
-  throw new Error('Not Implemented');
+export const getQuizzes = async (): Promise<Quiz[]> => {
+  const connection = await getConnection();
+
+  const quizQueryString = `
+  SELECT 
+    quiz.\`index\`,
+    quiz_item.\`index\` as itemIndex,
+    quiz.title,
+    quiz_item.content
+  FROM quiz
+  JOIN quiz_item ON quiz.id = quiz_item.quiz_id;
+  `;
+  const [queryResult] = await connection.query<RowDataPacket[]>(quizQueryString);
+
+  connection.release();
+
+  const result = queryResult
+    .reduce((original, { index, itemIndex, title, content }) => {
+      const quizzes = [...original];
+
+      if (!original[index]) {
+        quizzes[index] = { index, title, contents: [] };
+      }
+
+      quizzes[index].contents[itemIndex] = content;
+
+      return quizzes;
+    }, [] as Quiz[]);
+
+  return result;
 };
 
-export const getQuizByIndex = (index: number): Promise<Quiz | null> => {
-  throw new Error('Not Implemented');
+export const getQuizByIndex = async (index: number): Promise<Quiz | null> => {
+  const connection = await getConnection();
+
+  const quizQueryString = `
+  SELECT 
+    quiz.\`index\`,
+    quiz.title,
+    quiz_item.\`index\` as itemIndex,
+    quiz_item.content
+  FROM quiz
+  JOIN quiz_item ON quiz.id = quiz_item.quiz_id
+  WHERE quiz.\`index\` = ${index};
+  `;
+  const [queryResult] = await connection.query<RowDataPacket[]>(quizQueryString);
+
+  connection.release();
+
+  if (queryResult.length < 1) {
+    return null;
+  }
+
+  const result: Quiz = {
+    index: queryResult[0].index,
+    title: queryResult[0].title,
+    contents: queryResult
+      .sort(({ itemIndex: a }, { itemIndex: b }) => a - b)
+      .map(({ content }) => content),
+  };
+
+  return result;
 };
