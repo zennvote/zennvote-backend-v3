c50e1041bae47ca3b0db3b98f88668fb2e116bb0
XxshiftxX <contact@dev-shift.me>
2020-09-05 00:21:31 +0900

    FIX: google-docs infrastructure
    - migrateSong 함수 구현

--- src/infrastructure/google-docs/index.ts
+++ src/infrastructure/google-docs/index.ts
@@ -1,9 +1,13 @@
+import * as SongRepository from '@src/repository/Song';
+
 import * as sheets from './sheets';
 
 export const migrateProducer = async () => {
   throw new Error('Not Implemented');
 };
 
-export const migrateSong = async () => {
-  throw new Error('Not Implemented');
+export const migrateSong = async (season: number) => {
+  const songs = await sheets.getSeasonData(season);
+
+  await SongRepository.AddSongs(songs);
 };
