a0585136d400fde5dbb7610fbbebdb0a232707ff
shift <qjfrntop12@naver.com>
2020-09-17 21:41:12 +0900

    ADD: Statistics e2e test

--- test/e2e/Statistics.spec.ts
+++ test/e2e/Statistics.spec.ts
@@ -4,10 +4,17 @@ import { should } from 'chai';
 
 import app from '@src/interface/App';
 
+import * as VoteRepository from '@src/repository/Vote';
+import { EpisodeVoteField, StaticVoteField } from '@src/domain/value-object/VoteField';
+
+import { getVoteStatisticsSample } from './Statistics.util';
+
 should();
 
 describe('Statistics Test', () => {
   const request = supertest(app);
+  const episodeVoteFields: EpisodeVoteField[] = ['pitch', 'voice', 'funny', 'content', 'original', 'sleep'];
+  const staticVoteFields: StaticVoteField[] = ['unit', 'rookie', 'grow', 'master'];
 
   beforeEach(() => {
     sinon.restore();
@@ -16,10 +23,34 @@ describe('Statistics Test', () => {
   describe('GET /vote/statistics', () => {
     it('should return full statistics', async () => {
       // Arrange
+      const getEpisodeVoteStatistics = sinon.stub(VoteRepository, 'getEpisodeVoteStatistics');
+      const getStaticVoteStatistics = sinon.stub(VoteRepository, 'getStaticVoteStatistics');
+      episodeVoteFields.forEach((field) => {
+        getEpisodeVoteStatistics.withArgs(field).resolves(getVoteStatisticsSample(field));
+      });
+      staticVoteFields.forEach((field) => {
+        getStaticVoteStatistics.withArgs(field).resolves(getVoteStatisticsSample(field));
+      });
+      const sample = [...episodeVoteFields, ...staticVoteFields].reduce((acc, field) => {
+        acc[field] = getVoteStatisticsSample(field);
+
+        return acc;
+      }, {});
 
       // Act
+      const response = await request.get('/vote/statistics');
 
       // Assert
+      response.status.should.equal(200);
+      response.body.result.should.deep.equal(sample);
+
+      getEpisodeVoteStatistics.args.should.have.length(6);
+      getEpisodeVoteStatistics.args.map(([field]) => field)
+        .should.have.members(episodeVoteFields);
+
+      getStaticVoteStatistics.args.should.have.length(4);
+      getStaticVoteStatistics.args.map(([field]) => field)
+        .should.have.members(staticVoteFields);
     });
   });
 });
